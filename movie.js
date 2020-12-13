6a6048a9eb6d1a9779dd60f74a60092ee0d585fa
Zeyu Wu <wuzy@bu.edu>
2020-12-13 12:54:10 -0500

    ajex test

---
+++ movie.js
@@ -0,0 +1,54 @@
+$(document).ready(function () {
+    console.log("ready");
+    $("#btn").click(
+        function () {
+            console.log("button clicked");
+            makeRequest();
+        }
+    );
+
+});
+
+function makeRequest() {
+    let httpRequest = new XMLHttpRequest();
+    if (!httpRequest) {
+        alert("error: makeRequest");
+        return false;
+    }
+
+    httpRequest.onreadystatechange = function () {
+        if (httpRequest.readyState === XMLHttpRequest.DONE) {
+            if (httpRequest.status === 200) {
+                console.log('result', httpRequest.responseText);
+                let myArray = JSON.parse(httpRequest.responseText).movies;
+                if (Array.isArray(myArray)) {
+                    var tbl = $("<table/>").attr("id", "mytable");
+                    $("#result").append(tbl);
+                    var th = "<tr><th></th><th>Major</th><th>Type</th><th>Year Conferred</th></tr>";
+                    $("#mytable").append(th);
+                    myArray.forEach((element) => {
+                        console.log(element);
+                        var td1 = "<tr><td>" + element.movie.title + "</td>";
+                        var td2 = "<td>" + element.movie.director+ "</td>";
+                        var td3 = "<td>" + element.movie.star + "</td>";
+                        var td4 = "<td>" + element.movie.type + "</td></tr>";
+                        $('#mytable').append(td1 + td2 + td3 + td4);
+
+                    });
+
+                }
+            }
+            else {
+                console.log("Error: status");
+            }
+        } else {
+            console.log("Error: XMLHttpRequest");
+        }
+    };
+
+
+    httpRequest.open('GET', 'myCollegeDegrees.json', true);
+    console.log("open done");
+    httpRequest.send();
+    console.log("send done");
+}
